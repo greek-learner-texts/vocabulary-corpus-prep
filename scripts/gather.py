3060dcf4bbda13e6b486137b0f024feca9a562bd
James Tauber <jtauber@jtauber.com>
2025-08-02 17:59:48 -0400

    alignment and corrections to Gorman (especially Thucydides)

--- scripts/gather.py
+++ scripts/gather.py
@@ -68,6 +68,8 @@ def write_gorman_file(paths, work_id, group_id, group_label):
                         head = gchild.attrib.get("head")
                         if re.match(r"\[\d\]", form):
                             continue
+                        if gchild.attrib.get("artificial"):
+                            continue
                         print(group_id, work_id, subdoc, "-", word_id, "-", form, "-", "-", "-", postag, "-", lemma, sep="\t", file=g)
 
 
