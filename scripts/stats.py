5a5fdfa01434c13f2f81ed62e2a11e2cb70570b9
James Tauber <jtauber@jtauber.com>
2025-08-07 00:54:11 -0400

    updated counted for Glaux

--- scripts/stats.py
+++ scripts/stats.py
@@ -7,13 +7,14 @@ TAGGED_TEXTS_DIR = REPO_DIR / "tagged-texts"
 
 
 with open(REPO_DIR / "counts.tsv", "w") as counts_file:
-    print(f"AUTHOR\tWORK\tTAGGED\tOGA\tGORMAN", file=counts_file)
+    print(f"AUTHOR\tWORK\tTAGGED\tOGA\tGORMAN\tGLAUX", file=counts_file)
 
     for author_dir in sorted(TAGGED_TEXTS_DIR.iterdir()):
         for work_dir in sorted(author_dir.iterdir()):
             tagged = 0
             oga = 0
             gorman = 0
+            glaux = 0
             for version_path in work_dir.glob("*.tsv"):
                 author, work, version = version_path.stem.split(".")
                 tokens = version_path.read_text().splitlines()
@@ -23,5 +24,7 @@ with open(REPO_DIR / "counts.tsv", "w") as counts_file:
                     oga = len(tokens)
                 elif version == "gorman":
                     gorman = len(tokens)
-            print(f"{author_dir.name.split("_")[0]}\t{work_dir.name}\t{tagged}\t{oga}\t{gorman}", file=counts_file)
+                elif version == "glaux":
+                    glaux = len(tokens)
+            print(f"{author_dir.name.split("_")[0]}\t{work_dir.name}\t{tagged}\t{oga}\t{gorman}\t{glaux}", file=counts_file)
 
